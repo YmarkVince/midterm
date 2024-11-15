c6db38b075bf17e8d83ee5e1734da6f8f28c316f
YmarkVince <ymarkvincecabatbat@gmail.com>
2024-11-15 23:43:12 +0800

    removing nav bar

--- root/header.php
+++ root/header.php
@@ -1,10 +1,9 @@
-<!-- header.php -->
+
 <!DOCTYPE html> 
 <html lang="en"> 
 <head>
-    <meta charset="UTF-8"> <!-- Ensures that the character encoding is UTF-8 to support all characters -->
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- Makes the page responsive on mobile devices -->
-    <title>Student Management System</title> <!-- Sets the title of the webpage -->
+    <meta charset="UTF-8"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
     
     
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
@@ -12,22 +11,4 @@
 <body> 
 
     
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Student Management System</a> 
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="index.php">Login</a> 
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="dashboard.php">Dashboard</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="student/register.php">Register Student</a> 
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="subject/add.php">Add Subject</a> 
-                </li>
-            </ul>
-        </div>
-    </nav> <!-- End of navbar -->
+   
