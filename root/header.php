7d12f8c00a4fcfb5c312ee84a86294d4134b6bf2
YmarkVince <ymarkvincecabatbat@gmail.com>
2024-11-15 23:46:45 +0800

    removing nav bar in header

--- root/header.php
+++ root/header.php
@@ -12,22 +12,4 @@
 <body> 
 
     
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Student Management System</a> 
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="index.php">Login</a> 
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="dashboard.php">Dashboard</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="student/register.php">Register Student</a> 
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="subject/add.php">Add Subject</a> 
-                </li>
-            </ul>
-        </div>
-    </nav> <!-- End of navbar -->
+
